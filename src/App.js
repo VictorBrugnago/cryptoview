202df2a9e16cfef33d0771ef0e645781611b0766
Victor Brugnago <jv7@hotmail.com.br>
2021-11-05 02:59:29 -0400

    Created an Index and a base App.js

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <div className="app">
+            <div className="navbar">
+
+            </div>
+            <div className="main">
+
+            </div>
+            <div className="footer">
+
+            </div>
+        </div>
+    )
+};
+
+export default App;
