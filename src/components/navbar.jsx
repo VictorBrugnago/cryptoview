102588938d1d9df9be4a7d2074b9a81db76fdf47
Victor Brugnago <jv7@hotmail.com.br>
2021-11-06 14:48:46 -0400

    Created the menu items

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import {Typography, Avatar} from "antd";
+import {Menu, Typography, Avatar} from "antd";
 import {Link} from 'react-router-dom';
+import {HomeOutlined, MoneyCollectOutlined, BulbOutlined, FundOutlined} from "@ant-design/icons";
 
 import icon from "../images/cryptoview.png";
 
@@ -16,6 +17,25 @@ const Navbar = () => {
 
                 {/*</Button>*/}
             </div>
+
+            {/*Defining the menu*/}
+            <Menu theme="dark">
+                <Menu.Item icon={<HomeOutlined/>}>
+                    <Link to="/">Home</Link>
+                </Menu.Item>
+
+                <Menu.Item icon={<FundOutlined/>}>
+                    <Link to="/cryptocurrencies">Cryptocurrencies</Link>
+                </Menu.Item>
+
+                <Menu.Item icon={<MoneyCollectOutlined/>}>
+                    <Link to="/exchanges">Exchanges</Link>
+                </Menu.Item>
+
+                <Menu.Item icon={<BulbOutlined/>}>
+                    <Link to="/news">News</Link>
+                </Menu.Item>
+            </Menu>
         </div>
     )
 };
