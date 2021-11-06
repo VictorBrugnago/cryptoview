b9fb1990db51d87ab976b1cf3899e569827ad666
Victor Brugnago <jv7@hotmail.com.br>
2021-11-06 14:37:45 -0400

    Showing the navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
+import {Navbar} from './components';
+
 const App = () => {
     return (
         <div className="app">
             <div className="navbar">
-
+                <Navbar/>
             </div>
             <div className="main">
 
