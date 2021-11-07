ae1302241608df131632370b91fe57329ef034e6
Victor Brugnago <jv7@hotmail.com.br>
2021-11-06 22:45:22 -0400

    Added Cryptocurrencies and News into the homepage

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import millify from "millify";
 import {Typography, Row, Col, Statistic} from "antd";
+import {Link} from 'react-router-dom';
 
 
 import {useGetCryptosQuery} from "../services/cryptoApi";
+import {Cryptocurrencies, News} from "../components";
 
 const {Title} = Typography;
 
 const Homepage = () => {
-    const {data, isFetching} = useGetCryptosQuery();
+    const {data, isFetching} = useGetCryptosQuery(10);
     const globalStats = data?.data?.stats;
-
-    console.log(data);
 
     if (isFetching) return 'Loading...';
 
@@ -25,6 +25,18 @@ const Homepage = () => {
                 <Col span={12}><Statistic title="Total 24h Volume" value={millify(globalStats.total24hVolume)}/></Col>
                 <Col span={12}><Statistic title="Total Markets" value={millify(globalStats.totalMarkets)}/></Col>
             </Row>
+
+            <div className="home-heading-container">
+                <Title level={2} className="home-title">Top 10 Cryptocurrencies in the world</Title>
+                <Title level={3} className="show-more"><Link to="/cryptocurrencies">Show More</Link></Title>
+            </div>
+            <Cryptocurrencies simplified={true}/>
+
+            <div className="home-heading-container">
+                <Title level={2} className="home-title">Latest Crypto News</Title>
+                <Title level={3} className="show-more"><Link to="/news">Show More</Link></Title>
+            </div>
+            <News simplified />
         </>
     )
 };
