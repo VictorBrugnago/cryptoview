7a04b17e721d58cfb74b60dc36bc96e745442010
Victor Brugnago <jv7@hotmail.com.br>
2021-11-06 22:13:16 -0400

    Collecting values from the API

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,10 +1,31 @@
 import React from 'react';
+import millify from "millify";
+import {Typography, Row, Col, Statistic} from "antd";
+
+
+import {useGetCryptosQuery} from "../services/cryptoApi";
+
+const {Title} = Typography;
 
 const Homepage = () => {
+    const {data, isFetching} = useGetCryptosQuery();
+    const globalStats = data?.data?.stats;
+
+    console.log(data);
+
+    if (isFetching) return 'Loading...';
+
     return (
-        <div>
-            Homepage
-        </div>
+        <>
+            <Title level={2} className="heading">Global Crypto Stats</Title>
+            <Row>
+                <Col span={12}><Statistic title="Total Cryptocurrencies" value={globalStats.total}/></Col>
+                <Col span={12}><Statistic title="Total Exchanges" value={millify(globalStats.totalExchanges)}/></Col>
+                <Col span={12}><Statistic title="Total Market Cap" value={millify(globalStats.totalMarketCap)}/></Col>
+                <Col span={12}><Statistic title="Total 24h Volume" value={millify(globalStats.total24hVolume)}/></Col>
+                <Col span={12}><Statistic title="Total Markets" value={millify(globalStats.totalMarkets)}/></Col>
+            </Row>
+        </>
     )
 };
 
